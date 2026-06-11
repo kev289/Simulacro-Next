faed3a37278e8f1216e80fc79dee1e13cf0fe9b2
Kevin <kevinuribe114@gmail.com>
2026-06-11 06:50:25 -0500

    login service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,6 +2,10 @@ import connectDB from "../lib/mongodb";
 import { User } from "../models/User";
 import { authLib } from "../lib/auth";
 import { IUser } from "../types/IUser";
+import z from "zod";
+import { LoginValidation } from "../lib/validations";
+
+type LoginInput = z.infer<typeof LoginValidation>;
 
 export const userService = {
     register: async (data: IUser & { confirmPassword?: string }) => {
@@ -21,5 +25,40 @@ export const userService = {
         })
 
         return newUser;
-    }
+    },
+
+    login: async (data: LoginInput) => {
+        await connectDB();
+
+        const user = await User.findOne({ email: data.email });
+        if (!user) {
+            throw new Error("Invalido.")
+        }
+
+        const IsPasswordValid = await authLib.comparePassword(data.password, user.password)
+        if (!IsPasswordValid) {
+            throw new Error("Invalido")
+        }
+
+        const accessToken = await authLib.generateAccessToken({
+            userId: user._id.toString(),
+            email: user.email,
+            name: user.name
+        })
+
+        const refreshToken = await authLib.generateRefreshToken({
+            userId: user._id.toString(),
+            email: user.email,
+            name: user.name
+        })
+
+        return {
+            accessToken,
+            refreshToken,
+            user: {
+                name: user.name,
+                email: user.email
+            },
+        };
+    },
 };
