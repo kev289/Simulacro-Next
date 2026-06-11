56b4ce22237d237ba1fb7740b8283d8f3c8e1052
Kevin <kevinuribe114@gmail.com>
2026-06-10 23:11:37 -0500

    route register, missing add catch block

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,4 +1,29 @@
 import connectDB from "@/src/lib/mongodb";
+import { authLib } from "@/src/lib/auth";
+import { NextResponse, userAgent } from "next/server";
 import { RegisterValidation } from "@/src/lib/validations";
-import { NextResponse } from "next/server";
+import { User } from "@/src/models/User";
+
+export async function POST(req: Request) {
+    try {
+        await connectDB();
+        
+        const body = await req.json();
+        const validations = RegisterValidation.safeParse(body);
+
+        if(!validations.success) {
+            return NextResponse.json({error: "Datos invalidos" }, { status: 400 });
+        }
+
+        const hashPassword = await authLib.hashPassword(validations.data.password);
+
+        const user = await User.create({
+            name: validations.data.name,
+            email: validations.data.email,
+            password: hashPassword
+        });
+    } catch {
+
+    }
+} 
 
