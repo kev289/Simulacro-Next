0ac7d45676662e6e85f957020d2758c02f9bfecb
Kevin <kevinuribe114@gmail.com>
2026-06-11 08:17:09 -0500

    route login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,3 +1,39 @@
 import { NextResponse } from "next/server";
+import { LoginValidation } from "@/src/lib/validations";
+import { userService } from "@/src/services/user.service";
+import { access } from "fs";
 
+export async function GET(req: Request) {
+    try {
+        const body = await req.json();
 
+        const validations = LoginValidation.safeParse(body);
+        if(!validations.success) {
+            return NextResponse.json({ error: "Datos Invalidos"}, { status: 400 })
+        }
+
+        const { accessToken, refreshToken, user } = await userService.login(validations.data);
+
+        const response = NextResponse.json({
+            message: "Inicio de sesion exitoso",
+            user,
+            accessToken
+        }, { status: 200});
+
+        response.cookies.set("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            path: "/"
+        });
+
+        return response;  
+    } catch (error){
+        if( error instanceof Error) {
+            return NextResponse.json({ error: error.message }, { status: 401 })
+        }
+        return NextResponse.json({ error: "Error Fatal" }, { status: 500 });
+    }
+}
+
+ 
