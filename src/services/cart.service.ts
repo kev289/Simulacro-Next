dceb844071ed8f153e24b463034da0084b8fef17
Kevin <kevinuribe114@gmail.com>
2026-06-11 22:18:45 -0500

    service cart

---
+++ src/services/cart.service.ts
@@ -0,0 +1,20 @@
+import { Cart } from "../models/Cart";
+
+export const CartService = {
+    getCardService: async (userId: string) => {
+        const getCart = await Cart.find({ userId }).populate("productId")
+        return getCart;
+    },
+    addItemToCart: async (userId: string, productId: string, quantity: number) => {
+        const exist = await Cart.findOne({ userId, productId });
+
+        if (!exist) {
+            const newItem = await Cart.create({ userId, productId, quantity });
+            return newItem;
+        } else {
+            exist.quantity += quantity; 
+            await exist.save();        
+            return exist;
+        }
+    }
+}
